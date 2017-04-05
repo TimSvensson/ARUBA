edf25e14a96ca9f05cdc3c231fc86d4f022e0ff7
Haubir <jagheterhaubir@gmail.com>
2017-04-05 18:36:08 +0200

    Added some comments to the SortingList class

--- src/com/SortingList.java
+++ src/com/SortingList.java
@@ -11,29 +11,46 @@ public class SortingList {
     private List<Agent> listOfAgents = new ArrayList<Agent>();
     private int size;
 
+    /**
+     * Returns the list of Agents
+     * @return listOfAgents
+     */
     public List<Agent> getListOfAgents() {
         return listOfAgents;
     }
 
+    /**
+     * Returns the size of the list
+     * @return size
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * The constructor creates a new SortingList object by taking a list and adding it's elements onto it's own list
+     * @param list  - a list of Agents
+     */
     public SortingList(List<Agent> list) {
         this.size = 0;
         for (Agent a : list) {
-            this.listOfAgents.add(a);
-            this.size++;
+            this.addAgent(a);
         }
-        Collections.sort(listOfAgents);
     }
 
+    /**
+     * Adds an agent to the list and then sorts the list accordingly.
+     * @param a     - an Agent
+     */
     public void addAgent(Agent a) {
         this.listOfAgents.add(a);
         this.size++;
         Collections.sort(listOfAgents);
     }
 
+    /**
+     * Prints the contents of the list.
+     */
     public void printList() {
         for (Agent a : this.listOfAgents) {
             System.out.println("Agent " + a.getFirstName() + " has a travelTime of " + a.getTravelTime() + "");
