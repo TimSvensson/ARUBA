9758a1901e5d496e10a6416db393e43d2a112c5b
DeeAwesomeOne <Desiree.Bjorkman.4477@student.uu.se>
2017-04-07 14:14:44 +0200

    create a directory and one example file on test. Really basic

---
+++ tests/com/PositionTest.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by desiree on 2017-04-07.
+ */
+public class PositionTest {
+    public Geocoordinate geoTestHelp = new Geocoordinate(2222, 6666);
+    public Position positionTest = new Position(geoTestHelp, "", "", "", "", "", "");
+
+    @Test
+    public void getAddress() throws Exception{
+    assertEquals("", positionTest.getAddress());
+    }
+
+}
