9c74016584d19b2e560082f73d9451dd6733e10e
Haubir <jagheterhaubir@gmail.com>
2017-04-19 16:43:41 +0200

    SortingInterface.java: Basic functions have been declared although not complete.

--- src/com/Interface/SortingInterface.java
+++ src/com/Interface/SortingInterface.java
@@ -1,10 +1,47 @@
+/*
+ * %W% %E% Haubir Mariwani
+ *
+ * Version info
+ * First pass 4/19/17
+ *
+ * Prpoerty of Fasbros IT. Do not copy, rewrite, ues or distribute without permission from Fasbros IT
+ * Email: contact@fasbros.it
+ */
+
+
 package com.Interface;
 
 /**
- * Created by timsvensson on 11/04/17.
+ * Interface for the functionalities and duties of the sorting algorithm in this system.
+ * <p>
+ *
+ * </p>
+ *
+ * @author Haubir Mariwani <haubir.mariwani@fasbros.it>
+ * @version JDK 1.8
+ * @since JDK 1.8
  */
 public interface SortingInterface {
 
+    /**
+     * Adds an element to the list.
+     *
+     * @return true if the element was successfully added, otherwise false.
+     */
+    boolean addToList();
 
+    /**
+     * Returns the list.
+     *
+     * @return The list.
+     */
+    SortingList<T> getList();
+
+    /**
+     * Sorts the list according to the criteria.
+     *
+     * @return true if the list was successfully sorted, otherwise false.
+     */
+    boolean sortList();
 
 }
