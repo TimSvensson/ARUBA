179a7c2f8b175709570dfe66b84118b61c2e3541
Tim Svensson <svensson_tim@hotmail.se>
2017-04-24 08:48:20 +0200

    Implemented GHGeocodingAPI.

--- src/com/GraphHopperDirectionsAPI/GHGeocodingAPI.java
+++ src/com/GraphHopperDirectionsAPI/GHGeocodingAPI.java
@@ -12,8 +12,16 @@
 
 package com.GraphHopperDirectionsAPI;
 
+import com.Geocoordinate;
 import com.Interface.GeocodingInterface;
 import com.Position;
+
+import io.swagger.client.*;
+import io.swagger.client.model.*;
+import io.swagger.client.api.GeocodingApi;
+
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Class summary.
@@ -27,9 +35,107 @@ import com.Position;
  */
 public class GHGeocodingAPI implements GeocodingInterface {
 
+    public enum GHGeocodeProvider {DEFAULT, NOMINATIM, OPENCAGEDATA}
+
+    private String apiKey;
+    private GHGeocodeProvider ghGeocodeProvider = GHGeocodeProvider.DEFAULT;
+
+    private GeocodingResult geocodingResult;
+    private Position positionResult;
+    private int resultLimit = 0;
+
+    public Position getPositionResult() {
+        return positionResult;
+    }
+
+    public int getResultLimit() {
+        return resultLimit;
+    }
+
+    public void setResultLimit(int resultLimit) {
+        this.resultLimit = resultLimit;
+    }
+
+    public GHGeocodingAPI(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public void setGhGeocodeProvider(GHGeocodeProvider ghGeocodeProvider) {
+        this.ghGeocodeProvider = ghGeocodeProvider;
+    }
+
+    public GeocodingResult getGeocodingResult() {
+        return geocodingResult;
+    }
+
     @Override
     public boolean geocode(Position position) {
-        return false;
+
+        GeocodingApi apiInstance = new GeocodingApi();
+
+        // String | If you do forward geocoding, then this would be a textual description of the adress you are looking
+        // for. If you do reverse geocoding this would be in lat,lon.
+        String q = position.toString();
+
+        // String | Display the search results for the specified locale. Currently French (fr), English (en),
+        // German (de) and Italian (it) are supported. If the locale wasn't found the default (en) is used.
+        String locale = "en";
+
+        // Integer | Specify the maximum number of returned results
+        if (this.getResultLimit() <= 0) {
+            this.setResultLimit(1);
+        }
+
+        // Boolean | Set to true to do a reverse Geocoding request
+        Boolean reverse = false;
+
+        // String | The location bias in the format 'latitude,longitude' e.g. point=45.93272,11.58803
+        String point = "";
+
+        // String | Can be either, default, nominatim, opencagedata
+        String provider = "";
+        switch (this.ghGeocodeProvider) {
+            case DEFAULT:
+                provider = "default";
+                break;
+            case NOMINATIM:
+                provider = "nominatim";
+                break;
+            case OPENCAGEDATA:
+                provider = "opencagedata";
+                break;
+//            default:
+//                throw new IOException("ghGeocodeProvider is null");
+        }
+
+        try {
+            this.geocodingResult = apiInstance.geocodeGet(this.apiKey, q, locale, this.getResultLimit(), reverse, point, provider);
+//          System.out.println(geocodingResult);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling GeocodingApi#geocodeGet");
+            e.printStackTrace();
+            return false;
+        }
+
+        Location l = geocodingResult.getHits().get(0);
+        String street = "";
+        if (l.getStreet() != null) {
+            street += l.getStreet();
+            if (l.getHousenumber() != null) {
+                street += " " + l.getHousenumber();
+            }
+        }
+        positionResult = new Position(
+                new Geocoordinate(l.getPoint().getLat(), l.getPoint().getLng()),
+                street,
+                l.getCity(),
+                l.getState(),
+                l.getCountry(),
+                l.getPostcode(),
+                ""
+        );
+
+        return true;
     }
 
     @Override
